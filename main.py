48d735d535a9c55b5646202138fee3ef30e36bf5
drogtke958 <153301526+drogtke958@users.noreply.github.com>
2024-09-13 10:30:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-# Student name
-# Current date
+# Keily Drogt
+# 13 Sept 2024
 # String & Numeric Input 
 
 # Task 1
 # Comment
+name = input("What's your name?: ")
+career_tech = input("What's your class?: ")
 # Comment
-
+print("Hey "+name+" from "+career_tech)
 
 
 
 # Task 2
 # Comment
+age = "17"
+age_num = int(age)
+
+age_sentence = "The user is " + str(age_num) + "years old"
 # Comment
+print(age_sentence)
 
 
 
 
 # Task 3
 # Comment
+family_members = int(input("How many people are in your family?: "))
 # Comment
+print(family_members + "people in your family")
 
 
 
 
 # Task 4
 # Comment
+height = 
 # Comment
 
 
